b487cff268ba9e4ab3f4a1990d28a0844eee8752
Alexander Trusov <sanektttt@gmail.com>
2021-10-27 20:58:36 +0400

    pravki

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const name = () => {
+const name = () => {
   const user = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${user}!`);
 };
+export default name;
